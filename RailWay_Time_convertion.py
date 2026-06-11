2f226540fa71a584c8d8a7208d3d850a90d53088
Abi kishore <155071422+Kishore4647@users.noreply.github.com>
2026-06-11 22:12:44 +0530

    Add files via upload

---
+++ RailWay_Time_convertion.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    period = s[-2:]      # AM or PM
+    hour = int(s[:2])    # hour part
+    rest = s[2:-2]       # :mm:ss
+
+    if period == "AM":
+        if hour == 12:
+            hour = 0
+    else:  # PM
+        if hour != 12:
+            hour += 12
+
+    return f"{hour:02d}{rest}"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
